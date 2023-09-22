ee899d3076373097a8b1d0292167e2e801f637c4
Tegar Dimas Pradana <tegartpp@gmail.com>
2023-09-22 18:44:25 +0700

    Add files via upload

---
+++ distance_sensor.ino
@@ -0,0 +1,49 @@
+/*
+  Ultrasonic Sensor HC-SR04 and Arduino Tutorial
+
+  by Dejan Nedelkovski,
+  www.HowToMechatronics.com
+
+*/
+
+#include "Arduino.h"
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 17, 2);
+// defines pins numbers
+const int trigPin = 9;
+const int echoPin = 10;
+// defines variables
+long duration;
+float distance;
+void setup() {
+
+  lcd.begin(); // initialize the LCD
+  lcd.backlight();
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600); // Starts the serial communication
+}
+void loop() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+     lcd.print("Distance");
+     lcd.setCursor(0,1);
+     lcd.print(distance);
+     lcd.setCursor(6,1);
+     lcd.print(" cm");
+        delay(1000);
+        lcd.clear();
+}
